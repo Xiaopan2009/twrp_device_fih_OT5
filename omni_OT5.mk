4e8385eba9937dddf90d716ec2cc63c14f1dba15
Xiaopan2009 <Xiaopan2009@users.noreply.github.com>
2026-08-17 15:43:51 +0000

    fix: clean omni_OT5.mk - core_64_bit + full_base_telephony inherit

--- omni_OT5.mk
+++ omni_OT5.mk
@@ -5,10 +5,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
 
 # Inherit from OT5 device
 $(call inherit-product, device/fih/OT5/device.mk)
